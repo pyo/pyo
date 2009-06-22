9f94a29d35290b14a8743a914d31d9821c211ccb
matt.mongeau@digitalscientists.com <matt.mongeau@digitalscientists.com>
2009-06-22 13:00:52 -0400

    Remove facebook features from users controller. Refs #1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,3 @@
-class UsersController < ApplicationController
-  # facebooker methods
-  before_filter :set_facebook_session
-  helper_method :facebook_session
-  
-  # clearance
+class UsersController < ApplicationController   
   include Clearance::App::Controllers::UsersController
 end
