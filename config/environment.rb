bde056079d304e9b3b877d10d12ec7fa99f5d3c6
Lee Jones <lee.jones@digitalscientists.com>
2009-07-08 17:29:42 -0400

    Moonshine setup - added paperclip

--- config/environment.rb
+++ config/environment.rb
@@ -11,17 +11,18 @@ Rails::Initializer.run do |config|
   # Specify gems that this application depends on and have them installed with rake gems:install
   config.gem 'thoughtbot-factory_girl', :lib => 'factory_girl', :source => 'http://gems.github.com'
   config.gem 'thoughtbot-paperclip',    :lib => 'paperclip', :source => 'http://gems.github.com'
+  config.gem 'paperclip' # flags moonshine to install imagemagick
   config.gem 'thoughtbot-shoulda',      :lib => 'shoulda', :source => 'http://gems.github.com'
-  #config.gem 'mmangino-facebooker',     :lib => 'facebooker', :source => 'http://gems.github.com'
   config.gem 'giraffesoft-is_taggable', :lib => "is_taggable", :source => "http://gems.github.com"
   config.gem "thoughtbot-clearance",    :lib => 'clearance', :source => 'http://gems.github.com'
   config.gem 'beanstalk-client', :source => 'http://gems.github.com'
   config.gem 'right_aws'
   config.gem 'right_http_connection'
-  # config.gem 'capistrano'
   config.gem 'andand'
   config.gem 'haml'
-
+  config.gem 'capistrano'
+  #config.gem 'mmangino-facebooker',     :lib => 'facebooker', :source => 'http://gems.github.com'
+  
   # Activate observers that should always be running
   #config.active_record.observers = :covalence_notification_observer
   
