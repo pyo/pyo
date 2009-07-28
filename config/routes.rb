9bd678677f765c6b0559dbb5dad6703db5508388
matt.mongeau@digitalscientists.com <matt.mongeau@digitalscientists.com>
2009-07-28 17:56:43 -0400

    Route is now open

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :videos, :member => {:status => :post, :status_update => :get, :upload => :get, :done => :get}
+  map.resources :videos, :member => {:status => :post, :status_update => :any, :upload => :get, :done => :get}
 
   map.resources :blogs
 
