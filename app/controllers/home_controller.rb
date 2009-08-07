98c8f26737b3040504fdb18cb8dd459f6e66a765
matt@toastyapps.com <matt@toastyapps.com>
2009-08-07 18:05:00 -0400

    Format home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
-		@users = User.featured.all(:limit=>4,:order=>"RAND()")
+    @users = User.all(:limit => 30, :order => "RAND()")
+		@featured_users = User.featured.all(:limit=>4,:order=>"RAND()")
 		@posts = Blog.recent.super_user_posts.all(:limit=>4)
   end
 end
