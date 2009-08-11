abef00d87970e799f9d649269721ad56031b170d
matt@toastyapps.com <matt@toastyapps.com>
2009-08-11 12:27:53 -0400

    Update corners

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
   def index
     @users = User.all(:limit => 30, :order => "RAND()")
-		@featured_users = User.featured.all(:limit=>4,:order=>"RAND()")
+    @featured_users = User.all(:limit=>4,:order=>"RAND()")
+		#@featured_users = User.featured.all(:limit=>4,:order=>"RAND()")
 		@posts = Blog.recent.super_user_posts.all(:limit=>4)
   end
 end
