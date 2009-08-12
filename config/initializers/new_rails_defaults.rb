373531998869a8a4e75604bbdca6aa3348c27142
matt@toastyapps.com <matt@toastyapps.com>
2009-08-12 10:29:35 -0400

    Remove json dependency

--- config/initializers/new_rails_defaults.rb
+++ config/initializers/new_rails_defaults.rb
@@ -18,4 +18,4 @@ ActiveSupport.use_standard_json_time_format = true
 # if you're including raw json in an HTML page.
 ActiveSupport.escape_html_entities_in_json = false
 
-ActiveSupport::JSON.backend = 'JSONGem'
+# ActiveSupport::JSON.backend = 'JSONGem'
