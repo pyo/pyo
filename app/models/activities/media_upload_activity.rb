eddeb4385ec002062cae5235c43a2eebd9b11e76
matt@toastyapps.com <matt@toastyapps.com>
2009-09-03 10:39:40 -0400

    Blog status display update

--- app/models/activities/media_upload_activity.rb
+++ app/models/activities/media_upload_activity.rb
@@ -4,11 +4,19 @@ class MediaUploadActivity < Activity
     {:id => id, :message => message, :user => producer.profile.username, :created_at => ActionView::Base.new.time_ago_in_words(created_at)}.to_json
   end
   
+  def media_type
+    case payload_type
+    when 'Blog' then 'blog entry'
+    else payload.type.to_s.singularize.downcase
+    end
+  end
+  
   def icon
     case payload_type
     when 'Video' then 'icons/videos_16-plus.png'
     when 'Track' then 'icons/audio_16-plus.png'
     when 'Photo' then 'icons/photo_16-plus.png'
+    when 'Blog' then 'icons/blog_16-plus.png'
     end
   end
 end
