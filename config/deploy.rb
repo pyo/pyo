ee6a655553bd8b51be549b5c36d29499aeb10cf9
Christopher Burnett <chris.burnett@digitalscientists.com>
2009-09-11 15:06:18 -0400

    Fixed #110 fixing search

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,3 +6,8 @@ set :application, "PYO"
 require 'capsize'
 
 set :port, ENV['PORT'] || "30306"
+
+after :deploy    do
+  run "/usr/bin/rake -f #{deploy_to}/current/Rakefile ts:index RAILS_ENV=#{rails_env}"
+  run "/usr/bin/rake -f #{deploy_to}/current/Rakefile ts:rebuild RAILS_ENV=#{rails_env}"
+end
