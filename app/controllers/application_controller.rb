05f6e8030b67c5a3c245e08697c266eb0b51c536
matt@toastyapps.com <matt@toastyapps.com>
2009-09-15 10:18:25 -0400

    Remove authenticity token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@
 class ApplicationController < ActionController::Base
   include Clearance::App::Controllers::ApplicationController
   helper :all # include all helpers, all the time
+  skip_before_filter :verify_authenticity_token
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
