69dfc1a3a29c93c28878724d9c30f2b0eb8ea45b
Matthew Mongeau <matt@toastyapps.com>
2009-09-22 17:04:31 -0400

    Fix notification partial issue

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -2,7 +2,7 @@ module NotificationsHelper
   def display_notification(notification)
     # begin
       unless notification.type.blank?
-        render :partial => "activities/#{notification.type.to_s.downcase.gsub(/activity$/,'')}",:locals=>{:item=>notification}
+        render :partial => "activities/#{notification.type.to_s.underscore.gsub(/_activity$/,'')}",:locals=>{:item=>notification}
       else
         render :partial => "activities/default",:locals=>{:item=>notification}
       end
