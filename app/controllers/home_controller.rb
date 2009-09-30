19f8f2fe9ba1a3058f4e1fa3e875a8054d25b040
Chris Davis <jeffader@jeff-aders-imac.local>
2009-09-30 11:33:09 -0400

    contact and faq pages. Test #194 #195

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -17,4 +17,12 @@ class HomeController < ApplicationController
   def terms
     @title = "Terms of Use"
   end
+  
+  def contact
+    @title = "Contact Us"
+  end
+  
+  def faq
+    @title = "Frequently Asked Questions"
+  end
 end
