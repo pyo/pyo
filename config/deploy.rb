76b8313a646622b0062211ae0b279cc092e92613
Rails Machine build <ops@railsmachine.com>
2010-02-04 23:40:58 +0000

    Removed cap tasks for sphinx b/c they are now handled by the moonshine sphinx plugin and cron job

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,3 @@ set :stages, %w(staging production)
 set :default_stage, 'staging'
 require 'capistrano/ext/multistage' rescue 'YOU NEED TO INSTALL THE capistrano-ext GEM'
 
-after :deploy    do
-  run "/usr/bin/rake -f #{deploy_to}/current/Rakefile ts:index RAILS_ENV=#{rails_env}"
-  run "/usr/bin/rake -f #{deploy_to}/current/Rakefile ts:rebuild RAILS_ENV=#{rails_env}"
-end
