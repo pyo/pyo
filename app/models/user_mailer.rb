8030365934de267eb7d020780e48d83ab2aa2758
Dustin Gary <dustin@putyourselfon.com>
2010-05-02 01:46:12 -0400

    new user mailer config

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,21 +1,21 @@
 class UserMailer < ActionMailer::Base
   
   def direct_message(direct_message)  
-    subject      "You have a new Direct Message"  
+    subject      "You have a new Direct Message from #{direct_message.producer.name} on Put Yourself On..."  
     from         direct_message.producer.name  
     recipients   direct_message.consumer.email  
     body         :direct_message => direct_message
   end
   
   def new_follower(following)
-    subject    "You have a new follower"
+    subject    "#{following.parent.name} is now following you on Put Yourself On!"
     from       DO_NOT_REPLY
     recipients following.child
     body       :following => following
   end
 
   def photo_comment(photo, comment)
-    subject      "A comment has been posted to your photo"
+    subject      "#{comment.producer.name} posted a comment to your photo on Put Yourself On..."
     from         DO_NOT_REPLY
     recipients   photo.user.email
     body         :photo   => photo,
@@ -23,7 +23,7 @@ class UserMailer < ActionMailer::Base
   end
 
   def user_comment(user, comment)
-    subject      "A comment has been posted to your profile"
+    subject      "#{comment.producer.name} posted a comment to your profile on Put Yourself On..."
     from         DO_NOT_REPLY
     recipients   user.email
     body         :user    => user,
@@ -31,7 +31,7 @@ class UserMailer < ActionMailer::Base
   end
 
   def track_comment(track, comment)
-    subject      "A comment has been posted to your audio track"
+    subject      "#{comment.producer.name} posted a comment to your song on Put Yourself On..."
     from         DO_NOT_REPLY
     recipients   track.user.email
     body         :track    => track,
@@ -39,7 +39,7 @@ class UserMailer < ActionMailer::Base
   end
 
   def video_comment(video, comment)
-    subject      "A comment has been posted to your video"
+    subject      "#{comment.producer.name} posted a comment to your video on Put Yourself On..."
     from         DO_NOT_REPLY
     recipients   video.user.email
     body         :video   => video,
@@ -47,7 +47,7 @@ class UserMailer < ActionMailer::Base
   end
 
   def blog_comment(blog, comment)
-    subject      "A comment has been posted to your blog"
+    subject      "#{comment.producer.name} posted a comment to your blog on Put Yourself On..."
     from         DO_NOT_REPLY
     recipients   blog.user.email
     body         :blog    => blog,
@@ -55,21 +55,21 @@ class UserMailer < ActionMailer::Base
   end
 
   def like_email(like)
-    subject      "The user #{like.user.name} likes your #{like.media_type}"
+    subject      "The user #{like.user.name} likes your #{like.media_type}!"
     from         DO_NOT_REPLY
     recipients   like.media.user.email
     body         :like => like
   end
 
   def deny_group(group)
-    subject      "Your group request was denied"
+    subject      "Your group request for #{group.name} was denied..."
     from         DO_NOT_REPLY
     recipients   group.members.first.email
     body         :group => group
   end
 
   def approve_group(group)
-    subject      "Your group request was approved"
+    subject      "Your group request #{group.name} was approved!"
     from         DO_NOT_REPLY
     recipients   group.members.first.email
     body         :group => group
